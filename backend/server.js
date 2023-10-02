0da0368a8e8f24533b883325a4d4af7ca08865a1
mohamedfathi2710 <fathimohamed2710@gmail.com>
2023-10-02 10:57:49 +0100

    Intital Goals REST API

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const goalRoutes = require('./routes/goalRoutes');
 const { errorHandler } = require('./middleware/errorMiddleware');
+const colors = require('colors');
+const connectDB = require('./config/db');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
